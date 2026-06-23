ef604e1846d9cb2629b820e867ca31993b87258d
Samranakausar <kausarsamrana27@gmail.com>
2026-06-23 06:05:54 +0530

    Initial website upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function openMenu() {
+    document
+    .getElementById("sidebar")
+    .classList.add("active");
+}
+
+function closeMenu() {
+    document
+    .getElementById("sidebar")
+    .classList.remove("active");
+}
---
+++ js/videos.js
@@ -0,0 +1,5 @@
+const videos = [
+  "https://res.cloudinary.com/xxxxx/video/upload/v123/1.mp4",
+  "https://res.cloudinary.com/xxxxx/video/upload/v123/2.mp4",
+  "https://res.cloudinary.com/xxxxx/video/upload/v123/3.mp4"
+];
