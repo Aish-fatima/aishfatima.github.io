29fa8a909cc9a3606d9644915f56bd1367c81fc9
Aish-fatima <151252607+Aish-fatima@users.noreply.github.com>
2026-08-27 05:15:12 +0530

    Delete js/videos.js

--- js/videos.js
+++
@@ -1,5 +0,0 @@
-const videos = [
-  "https://res.cloudinary.com/xxxxx/video/upload/v123/1.mp4",
-  "https://res.cloudinary.com/xxxxx/video/upload/v123/2.mp4",
-  "https://res.cloudinary.com/xxxxx/video/upload/v123/3.mp4"
-];
